bab1a882708dea743f5bd83a0de8591a8da21cbf
TomRomeo <tom.romeo.doil@gmail.com>
2025-08-01 14:47:59 +0200

    feat: show expiry date in list view

--- internal/service/secrets_service.go
+++ internal/service/secrets_service.go
@@ -9,8 +9,8 @@ import (
 
 type SecretsService interface {
 	InspectTLSSecret(namespace, name string) ([]CertificateInfo, error)
-	ListTLSSecrets(namespace string) ([]domains.K8SResourceID, error)
-	ListTLSSecret(namespace, name string) (domains.K8SResourceID, error)
+	ListTLSSecrets(namespace string) ([]domains.SecretInfo, error)
+	ListTLSSecret(namespace, name string) (domains.SecretInfo, error)
 	RawInspectTLSSecret(namespace, name string) (string, string, error)
 }
 
@@ -30,7 +30,7 @@ func (s secretsService) InspectTLSSecret(namespace, name string) ([]CertificateI
 		return nil, fmt.Errorf("can not inspect TLS secret: %w", err)
 	}
 
-	certData, err := parseCertsFromString(string(secret.TLSCert))
+	certData, err := ParseCertsFromString(string(secret.TLSCert))
 
 	if err != nil {
 		return nil, fmt.Errorf("can not parse TLS secret: %w", err)
@@ -41,28 +41,21 @@ func (s secretsService) InspectTLSSecret(namespace, name string) ([]CertificateI
 	return parsedCert, nil
 }
 
-func (s secretsService) ListTLSSecrets(namespace string) ([]domains.K8SResourceID, error) {
+func (s secretsService) ListTLSSecrets(namespace string) ([]domains.SecretInfo, error) {
 	secrets, err := s.GetTLSSecrets(namespace)
 
 	if err != nil {
 		return nil, fmt.Errorf("can not list TLS secrets: %w", err)
 	}
-
-	var tlsSecretsNames []domains.K8SResourceID
-	for _, secret := range secrets {
-		tlsSecretsNames = append(tlsSecretsNames, domains.K8SResourceID{Name: secret.Name, Namespace: secret.Namespace})
-	}
-
-	return tlsSecretsNames, nil
+	return secrets, nil
 }
 
-func (s secretsService) ListTLSSecret(namespace, name string) (domains.K8SResourceID, error) {
+func (s secretsService) ListTLSSecret(namespace, name string) (domains.SecretInfo, error) {
 	secret, err := s.GetTLSSecret(namespace, name)
 	if err != nil {
-		return domains.K8SResourceID{}, fmt.Errorf("failed to get TLS secret %s in namespace %s: %w", name, namespace, err)
+		return domains.SecretInfo{}, fmt.Errorf("failed to get TLS secret %s in namespace %s: %w", name, namespace, err)
 	}
-
-	return domains.K8SResourceID{Name: secret.Name, Namespace: secret.Namespace}, nil
+	return secret, nil
 }
 
 func (s secretsService) RawInspectTLSSecret(namespace, name string) (cert string, key string, err error) {
